e76a62b2cb203aa096a52ecb6e301802b1642550
mercury45 <lking1231@mail.ru>
2025-02-21 13:52:48 +0300

    + postgres settings

--- cakestore/settings.py
+++ cakestore/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,8 +78,16 @@ WSGI_APPLICATION = "cakestore.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "cakestore",
+        "USER": "postgres",
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "5432",
+        # "PORT": "5434",
+        # "USER": "cakestore",
+        # "PASSWORD": "cakestore",
+
     }
 }
 
