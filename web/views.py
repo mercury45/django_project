f3516b526811a11df4dcd0dfa9504250f1f641bf
mercury45 <lking1231@mail.ru>
2025-02-22 17:48:44 +0300

    + base templates

--- web/views.py
+++ web/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, authenticate, login
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.utils.datetime_safe import datetime
+
+from web.forms import RegistrationForm, AuthForm
+
+User = get_user_model()
+
 
 # Create your views here.
+def main_view(request):
+    year = datetime.now().year
+    return render(request, 'web/main.html', {
+        "year" : year
+    })
+
+def registration_view(request):
+    form = RegistrationForm()
+    is_success = False
+    if request.method == 'POST':
+        form = RegistrationForm(data=request.POST)
+        if form.is_valid():
+            user = User(
+                username = form.cleaned_data['username'],
+                email = form.cleaned_data['email'],
+            )
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            is_success = True
+            print(form.cleaned_data)
+    return render(request, 'web/registration.html', {
+        "form" : form,
+        "is_success" : is_success,
+    })
+
+
+def auth_view(request):
+    form = AuthForm()
+    if request.method == 'POST':
+        form = AuthForm(data=request.POST)
+        if form.is_valid():
+            user = authenticate(**form.cleaned_data)
+            if user is None:
+                form.add_error(None, 'Введены неверные данные')
+            else:
+                login(request, user)
+                return redirect("main")
+    return render(request, 'web/auth.html', {"form":form})
+
